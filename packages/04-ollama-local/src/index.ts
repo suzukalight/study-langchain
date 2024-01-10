2c7cd52355e85911616c48b0ee7000c9df6da44e
suzukalight <mail@mkubara.com>
2024-01-10 23:46:36 +0900

    feat: add 04-ollama-local

---
+++ packages/04-ollama-local/src/index.ts
@@ -0,0 +1,43 @@
+import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { HNSWLib } from "@langchain/community/vectorstores/hnswlib";
+import { HuggingFaceTransformersEmbeddings } from "@langchain/community/embeddings/hf_transformers";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const run = async () => {
+  const loader = new CheerioWebBaseLoader(
+    "https://lilianweng.github.io/posts/2023-06-23-agent/"
+  );
+  const docs = await loader.load();
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkOverlap: 0,
+    chunkSize: 500,
+  });
+
+  const splitDocuments = await splitter.splitDocuments(docs);
+
+  const vectorstore = await HNSWLib.fromDocuments(
+    splitDocuments,
+    new HuggingFaceTransformersEmbeddings()
+  );
+
+  const retrievedDocs = await vectorstore.similaritySearch(
+    "What are the approaches to Task Decomposition?"
+  );
+
+  console.log(retrievedDocs[0]);
+};
+
+run();
+/*
+  Document {
+    pageContent: 'Task decomposition can be done (1) by LLM with simple prompting like "Steps for XYZ.\\n1.", "What are the subgoals for achieving XYZ?", (2) by using task-specific instructions; e.g. "Write a story outline." for writing a novel, or (3) with human inputs.',
+    metadata: {
+      source: 'https://lilianweng.github.io/posts/2023-06-23-agent/',
+      loc: { lines: [Object] }
+    }
+  }
+*/
